f20d12390fb9ec3787e1804f95b01941d9e68761
chuang091 <thomas@ultralytics.com>
2025-07-03 19:50:34 +0800

    ci: test

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,14 +4,23 @@ export default defineVitestConfig({
   test: {
     environment: 'nuxt',
     globals: true,
+    setupFiles: ['./tests/setup.ts'],
+    // 排除 E2E 測試
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/e2e/**',
+    ],
     environmentOptions: {
       nuxt: {
         domEnvironment: 'happy-dom',
         mock: {
           intersectionObserver: true,
           indexedDb: true,
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  },
 })
